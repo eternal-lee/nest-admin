49039d7e35339958774409814339597c6ae94b10
LiQiang <1176952591@qq.com>
2025-06-30 18:04:22 +0800

    feat: 设置全局的jwt模块并引入

--- src/stystem/auth/auth.module.ts
+++ src/stystem/auth/auth.module.ts
@@ -1,23 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { UserModule } from '../user/user.module'
-import { JwtModule, JwtModuleOptions } from '@nestjs/jwt'
 import { AuthController } from './auth.controller'
-import { jwtConstants } from 'src/common/jwt/constants'
 
 @Module({
-  imports: [
-    UserModule,
-    JwtModule.registerAsync({
-      useFactory(): JwtModuleOptions {
-        return {
-          global: true,
-          secret: jwtConstants.secret // Replace with your actual secret key
-          // signOptions: { expiresIn: '60s' } // Example sign options
-        }
-      }
-    })
-  ],
+  imports: [UserModule],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService]
