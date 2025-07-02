6c27e6760c2f5e478be975efa57b4e0038a6e8b7
ieternal <1176952591@qq.com>
2025-07-02 10:31:54 +0800

    feat: 新增QQ授权接口

--- src/common/thirdAuth/index.ts
+++ src/common/thirdAuth/index.ts
@@ -1,6 +1,8 @@
 const prodAuthKey = {
   client_github_id: 'Ov23livX7ijuSIBDdWHi',
-  client_github_secret: 'b109e59cc7a5ce1dae0f6863aa1e5eb74bf9320b'
+  client_github_secret: 'b109e59cc7a5ce1dae0f6863aa1e5eb74bf9320b',
+  // QQ
+  client_qq_id: '102796021'
 }
 
 export { prodAuthKey }
